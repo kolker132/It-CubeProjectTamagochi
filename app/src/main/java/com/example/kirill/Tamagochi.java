06262d4b35fef5be2677e3ff15b43c1a9d6118f9
kolker <kirill06nik@gmail.com>
2023-04-21 18:06:11 +0300

    tamagochi

--- app/src/main/java/com/example/kirill/Tamagochi.java
+++ app/src/main/java/com/example/kirill/Tamagochi.java
@@ -6,17 +6,20 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.view.SurfaceHolder;
 
 public class Tamagochi extends Thread {
     private SurfaceHolder surfaceHolder;
     private volatile boolean running = true;
     private Paint backgroundPaint = new Paint();
+    Paint paint = new Paint();
     private Bitmap bitmap;
     private int towardPointX = 550;
     private int towardPointY = 1500;
     int smileX = 450;
     int smileY = 1400;
+
     {
         backgroundPaint.setColor(Color.WHITE);
         backgroundPaint.setStyle(Paint.Style.FILL);
@@ -39,19 +42,24 @@ public class Tamagochi extends Thread {
         towardPointY += dy;
 
     }
-
+    int room = 0;
+    Rect r1;
+    Rect r2;
+    Rect r3;
     @Override
     public void run() {
         while (running) {
             Canvas canvas = surfaceHolder.lockCanvas();
+            r3 = new Rect(canvas.getWidth()-25, canvas.getHeight()/2, canvas.getWidth(), canvas.getHeight()/2 + 300);
+            r2 = new Rect(canvas.getWidth()/2+150, 0, canvas.getWidth()/ 3 + 25,25);
+            r1 = new Rect(0, canvas.getHeight()/2, 25, canvas.getHeight()/2 + 300);
             if (canvas != null) {
                 try {
-                    canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), backgroundPaint);
-                    canvas.drawBitmap(bitmap, smileX, smileY, backgroundPaint);
-                    if (smileX + bitmap.getWidth() / 2 < towardPointX) smileX += 5;
-                    if (smileX + bitmap.getWidth() / 2 > towardPointX) smileX -= 5;
-                    if (smileY + bitmap.getHeight() / 2 < towardPointY) smileY += 5;
-                    if (smileY + bitmap.getHeight() / 2 > towardPointY) smileY -= 5;
+                    if (room == 1){
+
+                    }else {
+                        mainroom(canvas);
+                    }
                 } finally {
                     surfaceHolder.unlockCanvasAndPost(canvas);
                 }
@@ -59,4 +67,18 @@ public class Tamagochi extends Thread {
             }
         }
     }
+
+    private void mainroom(Canvas canvas) {
+        canvas.drawRect(0, 0, canvas.getWidth(), canvas.getHeight(), backgroundPaint);
+        canvas.drawBitmap(bitmap, smileX, smileY, backgroundPaint);
+        if (smileX + bitmap.getWidth() / 2 < towardPointX) smileX += 5;
+        if (smileX + bitmap.getWidth() / 2 > towardPointX) smileX -= 5;
+        if (smileY + bitmap.getHeight() / 2 < towardPointY) smileY += 5;
+        if (smileY + bitmap.getHeight() / 2 > towardPointY) smileY -= 5;
+        paint.setColor(Color.GRAY);
+        canvas.drawRect(r1, paint);
+        canvas.drawRect(r2, paint);
+        canvas.drawRect( r3, paint);
+    }
+
 }
